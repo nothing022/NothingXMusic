085109c199f13973e1db8231aef1d07c079a8d1b
OG VENOM <90243435+OG-VENOM@users.noreply.github.com>
2023-03-25 11:36:24 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@Its_Star_Boi"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomeditz"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@Its_Star_Boi"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomeditz"),
                 ]
             )
         if OWNER:
