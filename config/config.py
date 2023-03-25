605ef1cfa89c04a4dcf2b726f48bd1715b8e082e
Rahul Gupta <125535875+nothing022@users.noreply.github.com>
2023-03-25 11:14:22 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -27,7 +27,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/its-star-boi/ShizukaXMusic",
+    "https://github.com/nothing022/NothingXMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/nothing022/NothingXMusic")
