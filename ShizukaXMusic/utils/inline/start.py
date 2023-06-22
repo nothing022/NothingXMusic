0de8eb198e86832aa70b76fb315493215ef6c1db
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-06-22 21:59:32 +0530

    Update start.py

--- ShizukaXMusic/utils/inline/start.py
+++ ShizukaXMusic/utils/inline/start.py
@@ -67,14 +67,14 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomeditz"),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomedits"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomeditz"),
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"https://youtube.com/@ogvenomedits"),
                 ]
             )
         if OWNER:
