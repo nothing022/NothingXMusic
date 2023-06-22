20149fa18bda9e91cc75d192f705c566567a8f81
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-06-22 21:30:32 +0530

    Update git.py

--- ShizukaXMusic/core/git.py
+++ ShizukaXMusic/core/git.py
@@ -38,7 +38,7 @@ def git():
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
-        repo = Repo()
+        repo = Repo("https://github.com/nothing022/NothingXMusic")
         LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
         LOGGER(__name__).info(f"Invalid Git Command")
