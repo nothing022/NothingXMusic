1b96ec67d0fd378155fa35f51b59dd7e38035079
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-06-25 21:32:29 +0530

    Update userbot.py

--- ShizukaXMusic/core/userbot.py
+++ ShizukaXMusic/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Best_FriendsFor_Ever")
-                await self.one.join_chat("Star_X_Network")
+                await self.one.join_chat("NothingBots")
+                await self.one.join_chat("NothingBotz")
             except:
                 pass
             assistants.append(1)
@@ -79,8 +79,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("Best_FriendsFor_Ever")
-                await self.two.join_chat("Star_X_Network")
+                await self.two.join_chat("NothingBots")
+                await self.two.join_chat("NothingBotz")
             except:
                 pass
             assistants.append(2)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("Best_FriendsFor_Ever")
-                await self.three.join_chat("Star_X_Network")
+                await self.three.join_chat("NothingBots")
+                await self.three.join_chat("NothingBotz")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("Best_FriendsFor_Ever")
-                await self.four.join_chat("Star_X_Network")
+                await self.four.join_chat("NothingBots")
+                await self.four.join_chat("NothingBotz")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("Best_FriendsFor_Ever")
-                await self.five.join_chat("Star_X_Network")
+                await self.five.join_chat("NothingBots")
+                await self.five.join_chat("NothingBotz")
             except:
                 pass
             assistants.append(5)
