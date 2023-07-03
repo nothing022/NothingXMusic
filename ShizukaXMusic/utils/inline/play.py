474b240e3eb1a6b7ae20e33a9fe0bcff320494a7
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-07-03 14:29:00 +0530

    Update play.py

--- ShizukaXMusic/utils/inline/play.py
+++ ShizukaXMusic/utils/inline/play.py
@@ -40,7 +40,7 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
                 callback_data=f"add_playlist {videoid}",
             ),
             InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ 👀", url=f"tg://settingsmycrushh"
+                text=" ᴏᴡɴᴇʀ 👀", url=f"https://t.me/Infinityuncounted01"
             ),
         ],
         [
@@ -74,7 +74,7 @@ def telegram_markup_timer(_, chat_id, played, dur):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ 👀", url=f"tg://settingsmycrushh"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ 👀", url=f"https://t.me/Infinityuncounted01"),
         ],
         [
             InlineKeyboardButton(
@@ -97,7 +97,7 @@ def stream_markup(_, videoid, chat_id):
                 text=_["PL_B_2"],
                 callback_data=f"add_playlist {videoid}",
             ),
-            InlineKeyboardButton(text="ᴏᴡɴᴇʀ 👀", url=f"tg://settingsmycrushh"),
+            InlineKeyboardButton(text="ᴏᴡɴᴇʀ 👀", url=f"https://t.me/Infinityuncounted01"),
             InlineKeyboardButton(text="ᴏᴡɴᴇʀ ", url=f""),
         ],
         [
