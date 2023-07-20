2f7f73c01e082fa979c04283f139ad0c82a2da60
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-07-20 17:57:13 +0530

    Update logger.py

--- ShizukaXMusic/utils/logger.py
+++ ShizukaXMusic/utils/logger.py
@@ -10,23 +10,17 @@ async def play_logs(message, streamtype):
         else:
             chatusername = "ᴩʀɪᴠᴀᴛᴇ ᴄʜᴀᴛ"
         logger_text = f"""
-**━━━━━━━━━━━━━━━**
-**💞 {MUSIC_BOT_NAME} ᴍᴜsɪᴄ ʟᴏɢs **
-**━━━━━━━━━━━━━━━**
-**🌹️ 𝐂𝐡𝐚𝐭 𝐍𝐚𝐌𝐞 : >** {message.chat.title} [`{message.chat.id}`]
-**━━━━━━━━━━━━━━━**
-**🥀 𝐍𝐚𝐌𝐞 : ›** {message.from_user.mention}
-**━━━━━━━━━━━━━━━**
-**🌸 𝐔𝐬𝐞𝐑𝐧𝐚𝐌𝐞 : ›** @{message.from_user.username}
-**━━━━━━━━━━━━━━━**
-**🌷 𝐈𝐃  : ›** `{message.from_user.id}`
-**━━━━━━━━━━━━━━━**
-**🌿 𝐂𝐡𝐚𝐭 𝐥𝐢𝐧𝐤: >** {chatusername}
-**━━━━━━━━━━━━━━━**
-**🌻 𝐒𝐞𝐀𝐫𝐜𝐇𝐞𝐝 𝐅𝐨𝐫:** {message.text}
-**━━━━━━━━━━━━━━━**
-**💐 𝐒𝐭𝐫𝐄𝐚𝐦 𝐓𝐲𝐏𝐞:** {streamtype}
-**━━━━━━━━━━━━━━━**"""
+**{MUSIC_BOT_NAME} ᴩʟᴀʏ ʟᴏɢɢᴇʀ**
+
+**ᴄʜᴀᴛ:** {message.chat.title} [`{message.chat.id}`]
+**ᴜsᴇʀ:** {message.from_user.mention}
+**ᴜsᴇʀɴᴀᴍᴇ:** @{message.from_user.username}
+**ɪᴅ:** `{message.from_user.id}`
+**ᴄʜᴀᴛ ʟɪɴᴋ:** {chatusername}
+
+**sᴇᴀʀᴄʜᴇᴅ ғᴏʀ:** {message.text}
+
+**sᴛʀᴇᴀᴍ ᴛʏᴩᴇ:** {streamtype}"""
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
